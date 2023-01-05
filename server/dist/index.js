b160ad965281d8cf52363faf75b7954704639bc1
gravitowl <kjeld.berendsen@gmail.com>
2023-01-05 01:43:20 +0100

    Update index.js

--- server/dist/index.js
+++ server/dist/index.js
@@ -37,4 +37,4 @@ app.get('/*', (req, res) => {
 app.listen(process.env.PORT, () => {
     console.log(`Listening on port ${process.env.PORT}!`);
 });
-https_1.default.createServer(options, app).listen(8443);
+https_1.default.createServer(options, app).listen(process.env.PORT);
